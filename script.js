78a2edac5422760b86ca2d8776678430eb1855f3
Raf C <130236858+raf-c@users.noreply.github.com>
2023-04-20 01:37:23 -0400

    Add console.log() statements for each case in playRound()

--- script.js
+++ script.js
@@ -10,35 +10,54 @@ function playRound(playerSelection, computerSelection) {
 
     switch (player) {
         case computer:
+            console.log("TIE");
             return "It's a TIE! Play again";
             break;
         case "rock":
             if (computer == "scissors") {
+                console.log("W,RbS");
                 return "You WIN! Rock beats Scissors";
             } 
             if (computer == "paper") {
+                console.log("L,PbR");
                 return "You LOSE! Paper beats Rock";
             }
             break;
         case "paper":
             if (computer == "rock") {
+                console.log("W,PbR");
                 return "You WIN! Paper beats Rock";
             } 
             if (computer == "scissors") {
+                console.log("L,SbP");
                 return "You LOSE! Scissors beats Paper";
             }
             break;
         case "scissors":
             if (computer == "paper") {
+                console.log("W,SbP");
                 return "You WIN! Scissors beats Paper";
             } 
             if (computer == "rock") {
+                console.log("L,RbS");
                 return "You LOSE! Rock beats Scissors";
             }
             break;
     }
-   
-
 }
 
-/*getComputerChoice();*/
+function runGame() {
+    i = 5
+    while (i!=1) {
+        ps = prompt("Enter your move");
+        cs = getComputerChoice();
+        playRound(ps,cs);
+        i--;
+    }
+}
+   
+
+
+
+//getComputerChoice();
+//runGame();
